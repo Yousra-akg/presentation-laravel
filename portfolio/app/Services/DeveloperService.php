c0f09d9bd88d2f7ff997ff7667c4a8126a2f0f59
Yousra-akg <akagouyoussra@gmail.com>
2025-10-17 10:21:05 +0200

    Ajout de prototype

---
+++ portfolio/app/Services/DeveloperService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Services;
+
+class DeveloperService
+{
+    public function getProfile()
+    {
+        return [
+            'nom' => 'Yousra Akajou',
+            'role' => 'Développeuse Fullstack',
+            'bio' => 'Passionnée par le développement web et les nouvelles technologies.',
+            'email' => 'akajouyoussra@gmail.com',
+        ];
+    }
+}
